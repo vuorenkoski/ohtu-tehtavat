063b66e910cdab0e7025817f28a31bc34f283ddc
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2020-11-21 13:56:31 +0200

    Viikko5 - tehtävä4

---
+++ viikko5/tehtava4/LaskinFX11/src/main/java/laskin/Erotus.java
@@ -0,0 +1,30 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Erotus extends Komento {
+
+    public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        try {
+            int arvo = Integer.parseInt(syotekentta.getText());
+            sovellus.miinus(arvo);
+            int laskunTulos = sovellus.tulos();
+            syotekentta.setText("");
+            tuloskentta.setText("" + laskunTulos);
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
---
+++ viikko5/tehtava4/LaskinFX11/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,28 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Nollaa extends Komento {
+
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        try {
+            sovellus.nollaa();
+            int laskunTulos = sovellus.tulos();
+            syotekentta.setText("");
+            tuloskentta.setText("" + laskunTulos);
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
