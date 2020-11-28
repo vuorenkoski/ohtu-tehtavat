98e9a7c8e80b94a561fff03913d637c704200cbb
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2020-11-28 13:57:54 +0200

    viikko6 - tehtava4

--- viikko6/tehtava2/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/tehtava2/QueryLanguage/src/main/java/statistics/Main.java
@@ -7,18 +7,10 @@ public class Main {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-        
-        System.out.println(stats.matches(new All()).size());
-
-        
-        Matcher m = new And(
-            new HasAtLeast(50, "points"),
-            new Or( 
-                new PlaysIn("NYR"),
-                new PlaysIn("NYI"),
-                new PlaysIn("BOS")
-            )
-        );
+        QueryBuilder query = new QueryBuilder();
+        Matcher m = query.playsIn("NYR")
+                     .hasAtLeast(5, "goals")
+                     .hasFewerThan(10, "goals").build();
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
